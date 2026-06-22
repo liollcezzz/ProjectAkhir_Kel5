5b1277fd7bfdd8f89c9b8f314db213fc6b49b48b
Tesalonika Br Lumban <lonikatesa743@gmail.com>
2026-06-22 19:37:07 +0700

    feat: membuat user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void {
+        $defs = [
+            ['admin',    'Admin Utama',  'admin@aksesoria.test'],
+            ['cashier',  'Kasir Toko',   'kasir@aksesoria.test'],
+            ['warehouse','Staff Gudang', 'gudang@aksesoria.test'],
+            ['customer', 'Pelanggan',    'customer@aksesoria.test'],
+        ];
+        foreach ($defs as [$role, $name, $email]) {
+            User::updateOrCreate(
+                ['email' => $email],
+                ['name'=>$name,'role'=>$role,'password'=>Hash::make('password')]
+            );
+        }
+    }
+}
